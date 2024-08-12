222d8eb8d256b6cf58aff6e81bc47cce66b995f2
Nihal Kumar <kumarnihal1947@gmail.com>
2024-08-13 01:48:03 +0530

    initial commit

--- Backend/index.js
+++ Backend/index.js
@@ -4,7 +4,7 @@ import mongoose from "mongoose";
 import cors from"cors";
 //import connectToDB from "./db/connectToDB.js";
 import cookieParser from "cookie-parser";
-//import path from "path";
+import path from "path";
 //import authRoutes from "./routes/auth.routes.js";
 import userRoute from "./routes/user.route.js";
 import messageRoute from "./routes/message.route.js";
@@ -33,14 +33,14 @@ app.use("/api/user", userRoute);
 app.use("/api/message", messageRoute);
 
 //-----------code for deployment--------
-// if(process.env.NODE_ENV === 'production'){
-//   const dirPath = path.resolve();
+if(process.env.NODE_ENV === 'production'){
+  const dirPath = path.resolve();
 
-//   app.use(express.static("./Frontend/dist"));
-//   app.get("*", (req, res) => {
-//     res.sendFile(path.resolve(dirPath, "./Frontend/dist", "index.html"));
-//   });
-// }
+  app.use(express.static("./Frontend/dist"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(dirPath, "./Frontend/dist", "index.html"));
+  });
+}
 
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
