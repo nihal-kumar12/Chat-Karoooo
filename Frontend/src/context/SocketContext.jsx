423fb20a2ab6142f25f0055f3c9273d796a72593
Nihal Kumar <108837037+nihal-kumar12@users.noreply.github.com>
2024-08-15 04:01:57 +0530

    Update SocketContext.jsx

--- Frontend/src/context/SocketContext.jsx
+++ Frontend/src/context/SocketContext.jsx
@@ -17,7 +17,7 @@ export const SocketProvider=({children})=>{
 
     useEffect(()=>{
         if(authUser){
-            const socket=io("http://localhost:4005",{
+            const socket=io("https://chat-karoo.onrender.com",{
                 query:{
                     userId:authUser.user._id
                 },
